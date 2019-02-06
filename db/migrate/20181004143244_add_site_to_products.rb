70330d6a74dd9e3657e784520b3250e719986cbd
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2019-02-06 15:08:38 -0300

    Update 20181004143244_add_site_to_products.rb

--- db/migrate/20181004143244_add_site_to_products.rb
+++ db/migrate/20181004143244_add_site_to_products.rb
@@ -1,5 +1,6 @@
 class AddSiteToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :mercadolibre_rails_products, :mercadolibre_rails_site, foreign_key: true
+    add_reference :mercadolibre_rails_products, :mercadolibre_rails_site, foreign_key: true,
+                  index: { name: 'mercadolibre_products_on_seller_id' }
   end
 end
