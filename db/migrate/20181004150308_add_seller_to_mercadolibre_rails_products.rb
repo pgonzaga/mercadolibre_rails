25962423b6df0c566826265e148181c53f80c5ef
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2019-02-06 15:00:56 -0300

    Update 20181004150308_add_seller_to_mercadolibre_rails_products.rb

--- db/migrate/20181004150308_add_seller_to_mercadolibre_rails_products.rb
+++ db/migrate/20181004150308_add_seller_to_mercadolibre_rails_products.rb
@@ -1,5 +1,5 @@
 class AddSellerToMercadolibreRailsProducts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :mercadolibre_rails_products, :seller, foreign_key: true
+    add_reference :mercadolibre_rails_products, :mercadolibre_rails_seller, foreign_key: true
   end
 end
