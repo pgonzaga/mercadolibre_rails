0c7237daa9299c9fddde20227606be7bfc929ffd
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2019-02-06 15:12:42 -0300

    Update 20181022135655_add_site_id_to_mercadolibre_rails_sellers.rb

--- db/migrate/20181022135655_add_site_id_to_mercadolibre_rails_sellers.rb
+++ db/migrate/20181022135655_add_site_id_to_mercadolibre_rails_sellers.rb
@@ -2,6 +2,7 @@ class AddSiteIdToMercadolibreRailsSellers < ActiveRecord::Migration[5.2]
   def change
     add_reference :mercadolibre_rails_sellers, :mercadolibre_rails_site, foreign_key: true,
                   index: { name: 'mercadolibre_sellers_on_site_id' }
-    add_index :mercadolibre_rails_sellers, %i[mercadolibre_id site_id], name: 'ml_id_site_id_on_sellers'
+    add_index :mercadolibre_rails_sellers, %i[mercadolibre_id mercadolibre_rails_site_id],
+              name: 'ml_id_site_id_on_sellers'
   end
 end
